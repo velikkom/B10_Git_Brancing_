d3b35b804be9cfdd455193d7422f85590efaef57
Veli Kara <velikkom@hotmail.com>
2022-12-08 20:00:50 +0300

    added negative testing

--- src/TestCases/tc_001.java
+++ src/TestCases/tc_001.java
@@ -3,6 +3,10 @@ package TestCases;
 public class tc_001 {
     public static void main(String[] args) {
         // testing something
-// more testing
+        // more testing
+        // negative testing
+        //negative testing
+
+
     }
 }
